756f8dec6ee15c88555f00b76510a0fe28a550a6
Benjamin <br872788@gmail.com>
2023-12-12 11:19:39 -0600

    modificacion de app

--- pap.py
+++ pap.py
@@ -19,7 +19,7 @@ fig = px.histogram(car_data, x="odometer")
 # mostrar un gráfico Plotly interactivo
 st.plotly_chart(fig, use_container_width=True)
 
-build_scatter: st.checkbox('Construir un scatter plot')
+build_scatter = st.checkbox('Construir un scatter plot')
 
 if build_scatter: # al hacer clic en el botón
  # escribir un mensaje
